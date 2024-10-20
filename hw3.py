70717257522c98a2b46912b7caefaec238e477e1
swagsarthak <75614066+swagsarthak@users.noreply.github.com>
2024-10-20 14:56:07 -0400

    Add files via upload

---
+++ hw3.py
@@ -0,0 +1,118 @@
+# homework 4
+# goal: ranked retrieval, PageRank, crawling
+# exports:
+#   student - a populated and instantiated cs547.Student object
+#   PageRankIndex - a class which encapsulates the necessary logic for
+#     indexing and searching a corpus of text documents and providing a
+#     ranked result set
+
+# ########################################
+# first, create a student object
+# ########################################
+
+import cs547
+MY_NAME = "Sarthak Swain"
+MY_ANUM  = 901036243 # put your UID here
+MY_EMAIL = "sswain@wpi.edu"
+
+# the COLLABORATORS list contains tuples of 2 items, the name of the helper
+# and their contribution to your homework
+COLLABORATORS = [ 
+    ('No collab')
+    ]
+
+# Set the I_AGREE_HONOR_CODE to True if you agree with the following statement
+# "An Aggie does not lie, cheat or steal, or tolerate those who do."
+I_AGREE_HONOR_CODE = False
+
+# this defines the student object
+student = cs547.Student(
+    MY_NAME,
+    MY_ANUM,
+    MY_EMAIL,
+    COLLABORATORS,
+    I_AGREE_HONOR_CODE
+    )
+
+
+# ########################################
+# now, write some code
+# ########################################
+
+import bs4 as BeautifulSoup  # you will want this for parsing html documents
+
+
+# our index class definition will hold all logic necessary to create and search
+# an index created from a web directory
+#
+# NOTE - if you would like to subclass your original Index class from homework
+# 1 or 2, feel free, but it's not required.  The grading criteria will be to
+# call the index_url(...) and ranked_search(...) functions and to examine their
+# output.  The index_url(...) function will also be examined to ensure you are
+# building the index sanely.
+
+class PageRankIndex(object):
+    def __init__(self):
+        # you'll want to create something here to hold your index, and other
+        # necessary data members
+        
+        pass
+
+    # index_url( url )
+    # purpose: crawl through a web directory of html files and generate an
+    #   index of the contents
+    # preconditions: none
+    # returns: num of documents indexed
+    # hint: use BeautifulSoup and urllib
+    # parameters:
+    #   url - a string containing a url to begin indexing at
+    def index_url(self, url):
+        # ADD CODE HERE
+
+        return 0
+
+    # tokenize( text )
+    # purpose: convert a string of terms into a list of terms 
+    # preconditions: none
+    # returns: list of terms contained within the text
+    # parameters:
+    #   text - a string of terms
+    def tokenize(self, text):
+        # ADD CODE HERE
+
+        return []
+
+    # ranked_search( text )
+    # purpose: searches for the terms in "text" in our index and returns
+    #   AND results for highest 10 ranked results
+    # preconditions: .index_url(...) has been called on our corpus
+    # returns: list of tuples of (url,PageRank) containing relevant
+    #   search results
+    # parameters:
+    #   text - a string of query terms
+    def ranked_search(self, text):
+        # ADD CODE HERE
+        return []
+
+
+# now, we'll define our main function which actually starts the indexer and
+# does a few queries
+def main(args):
+    print(student)
+    index = PageRankIndex()
+    url = 'http://web.cs.wpi.edu/~kmlee/cs547/new10/index.html'
+    num_files = index.index_url(url)
+    search_queries = (
+       'palatial', 'college ', 'palatial college', 'college supermarket', 'famous aggie supermarket'
+        )
+    for q in search_queries:
+        results = index.ranked_search(q)
+        print("searching: %s -- results: %s" % (q, results))
+
+
+# this little helper will call main() if this file is executed from the command
+# line but not call main() if this file is included as a module
+if __name__ == "__main__":
+    import sys
+    main(sys.argv)
+
